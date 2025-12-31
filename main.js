c367ea4c9f2b6fceee0501b783d6168333e30d89
luisenriqueH <luisenrique@hernandez.com.ve>
2025-12-30 23:33:26 -0400

    Primera piedra

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("¡Hola, mundo!");
+console.log("Este es un programa JavaScript sencillo.");
+console.log("Imprime varias líneas en la consola.");
+console.log("Cada línea se imprime usando una declaración console.log separada.");
